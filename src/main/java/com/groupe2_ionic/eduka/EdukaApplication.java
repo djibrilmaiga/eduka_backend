4b5a5cbfb23cca9ed85bac4641afc9f97b59b937
Djibril Maïga <maigadjibril444@gmail.com>
2025-09-21 23:22:01 +0000

    Premier commit

---
+++ src/main/java/com/groupe2_ionic/eduka/EdukaApplication.java
@@ -0,0 +1,13 @@
+package com.groupe2_ionic.eduka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdukaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe2_ionic/eduka/EdukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe2_ionic.eduka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
